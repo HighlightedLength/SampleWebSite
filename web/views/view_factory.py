4250568e52e89f737e3284c50f3569792325b790
Sushrut Palatkar <sushrut619@gmail.com>
2016-09-25 15:53:31 -0400

    reinstall virtualenv

---
+++ web/views/view_factory.py
@@ -0,0 +1,15 @@
+import razor
+
+class ViewFactory:
+    def __init__(self):
+        self.cache = {}
+
+    def build(self, template_path, model):
+        if not(template_path in self.cache):
+            with open(template_path) as newFile:
+                doc = newFile.read()
+            self.cache[template_path] = pyrazor.Parse(doc)
+
+        template = self.cache.get(template_path)
+
+        return template.Render(doc,model)
